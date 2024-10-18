4942dfce935c3db03713a453e3628171d3ff0150
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-10-18 12:14:23 +0530

    Added: 2nd qr code generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import "./App.css";
-
 function App() {
-  return (
-    <div className="App">
-      <h1>QR Code Generator</h1>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
